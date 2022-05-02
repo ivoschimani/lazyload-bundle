04865bd0dd47f1d03f327a3a82e284b911b443de
ivo <ivo.schimani@lupcom.de>
2022-05-03 00:05:16 +0200

    fixed plugin.php

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -3,7 +3,7 @@
 namespace Ivo\LazyloadBundle\ContaoManager;
 
 use Contao\CoreBundle\ContaoCoreBundle;
-use Lupcom\LazyloadBundle\LazyloadBundle;
+use Ivo\LazyloadBundle\LazyloadBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
